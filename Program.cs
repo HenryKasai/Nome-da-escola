cc6912a3f25319a3302a1d130dfc685d26abee22
HenryKasai <henrykasai1775@gmail.com>
2020-02-28 20:35:06 -0300

    Solução de exercício

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Nome_da_escola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("============");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("============");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Nome-da-escola
 Programa que apresenta o nome da escola
+
+Solução do exercício:
+
+Faça um programa que exiba o nome da sua escola: Etec Adolpho Berezin.
